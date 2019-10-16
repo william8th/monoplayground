c8ac78af67d9773c26f8cea549a0651f698f455c
William Heng <william.heng@zopa.com>
2019-10-16 16:38:34 +0100

    WIP

--- resilience4j/src/main/kotlin/com/williamheng/resilience4j/Resilience.kt
+++ resilience4j/src/main/kotlin/com/williamheng/resilience4j/Resilience.kt
@@ -1,10 +1,9 @@
 package com.williamheng.resilience4j
 
-import io.github.resilience4j.retry.Retry
 import io.github.resilience4j.retry.RetryConfig
 import io.github.resilience4j.retry.RetryRegistry
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.get
 import io.ktor.client.response.HttpResponse
 import io.ktor.http.HttpStatusCode
@@ -12,22 +11,43 @@ import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import java.time.Duration
 
-class Resilience(private val url: String) {
+fun main() {
+    val resilience = Resilience()
+    resilience.retry()
+}
 
-    private val client = HttpClient(HttpClientEngine) {
-        install(JsonFeature)
-    }
+class Resilience {
+
     private val log = LoggerFactory.getLogger(this.javaClass)
 
-    fun hitWithRetry() {
+    private val client = HttpClient {
+        install(JsonFeature)
+    }
 
-        val retryConfig = RetryConfig.custom<HttpResponse>()
-                .maxAttempts(2)
-                .waitDuration(Duration.ofMillis(1000))
-                .retryOnResult { response -> response.status != HttpStatusCode.OK }
-                .build()
+    val retryConfig = RetryConfig.custom<HttpResponse>()
+        .maxAttempts(2)
+        .waitDuration(Duration.ofMillis(1000))
+        .retryOnResult { response -> response.status != HttpStatusCode.OK }
+        .build()
 
-        val retryRegistry = RetryRegistry.of(retryConfig)
+    val retryRegistry = RetryRegistry.of(retryConfig)
+
+    fun retry() {
+        runBlocking {
+            val retry = retryRegistry.retry("foobar")
+            retry.executeCallable {
+                val retryAttempt = retry.metrics.numberOfFailedCallsWithRetryAttempt
+
+                log.info("Retrying $retryAttempt")
+                if (retryAttempt < 10) {
+                    throw Exception("Deliberate failure")
+                }
+            }
+        }
+    }
+
+    fun hitWithRetry(url: String) {
+
         retryRegistry.retry("a-name").executeCallable {
             runBlocking {
                 log.info("Sending GET request for /api")
@@ -36,6 +56,4 @@ class Resilience(private val url: String) {
             }
         }
     }
-}
-
 }
